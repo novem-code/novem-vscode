54bf0702e36ee3baba61e2e3a64d0d7e71147d58
Sondov Engen <sondove@ifi.uio.no>
2023-09-29 16:36:39 +0200

    remove compilation artifacts

--- novem_web_view/App.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = require("react");
-const App = () => {
-    return <div>Hello World from Novem Web View!</div>;
-};
-exports.default = App;
-//# sourceMappingURL=App.js.map
--- novem_web_view/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = require("react");
-const react_dom_1 = require("react-dom");
-const App_1 = require("./App");
-require("./styles.css");
-react_dom_1.default.render(<App_1.default />, document.getElementById('root'));
-//# sourceMappingURL=index.js.map
